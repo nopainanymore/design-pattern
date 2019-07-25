730e111b7aba35aabb3b8024f879b544dafc43c6
Lee875083146 <vbof53@163.com>
2019-07-25 23:16:46 +0800

    simple factory finished

--- src/main/java/com/nopainanymore/designpattern/creation/simplefactory/MacComputer.java
+++ src/main/java/com/nopainanymore/designpattern/creation/simplefactory/MacComputer.java
@@ -1,6 +1,5 @@
 package com.nopainanymore.designpattern.creation.simplefactory;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;

e6ce5b5b492ee38e3db962f34b0e3f63f8004bfe
Lee875083146 <vbof53@163.com>
2019-08-01 22:43:43 +0800

    init adapter

---
+++ src/main/java/com/nopainanymore/designpattern/constructure/adapter/Client.java
@@ -0,0 +1,10 @@
+package com.nopainanymore.designpattern.constructure.adapter;
+
+/**
+ * design-pattern: Client
+ *
+ * @author NoPainAnymore
+ * @date 2019-08-01 22:30
+ */
+public class Client {
+}

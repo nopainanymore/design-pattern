0eb311eaeec7bcedabe87f7a98956f57567d0d42
NoPainAnyMore <vbof53@163.com>
2019-11-19 19:54:28 +0800

    explain InnerClass Singleton

--- src/main/java/com/nopainanymore/designpattern/creation/singleton/InnerClass.java
+++ src/main/java/com/nopainanymore/designpattern/creation/singleton/InnerClass.java
@@ -11,6 +11,11 @@ public class InnerClass {
     private InnerClass() {
     }
 
+    // 当getInstance方法第一次被调用的时候，它第一次读取SingletonHolder.instance，内部类SingletonHolder类得到初始化；
+    // 而这个类在装载并被初始化的时候，会初始化它的静态域，从而创建Singleton的实例，
+    // 由于是静态的域，因此只会在虚拟机装载类的时候初始化一次，并由虚拟机来保证它的线程安全性。
+    // 多线程并发时，JVM会保证只有一个线程执行类对初始化
+
     private static class InstanceHolder {
         private static final InnerClass INSTANCE = new InnerClass();
     }
